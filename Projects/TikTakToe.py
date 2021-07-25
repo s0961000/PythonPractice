ad7a01819dea483671b4186e192251b95b00944c
s0961000 <s0961000@monmouth.edu>
2021-07-25 12:58:29 -0400

    Updating TikTakToe

--- Projects/TikTakToe.py
+++ Projects/TikTakToe.py
@@ -18,12 +18,13 @@ def print_board(b):
         print("")
         for elem in row:
             print(f"{elem} ", end="")
+    print()
 
 
 def get_input():
     x = int(input("X: "))
     y = int(input("Y: "))
-    print(f"You selected: ({x},{y})")
+    # print(f"You selected: ({x},{y})")
     return x, y
 
 
@@ -44,22 +45,73 @@ def check_valid(x, y, board):
 
 
 def check_winner(board):
+    # Checking Rows for Winner
+    if board[0][0] == board[0][1] == board[0][2] and board[0][0] != "-":
+        return board[0][0]
+    if board[1][0] == board[1][1] == board[1][2] and board[1][0] != "-":
+        return board[1][0]
+    if board[2][0] == board[2][1] == board[2][2] and board[2][0] != "-":
+        return board[2][0]
+    # Checking Columns for Winner
+    if board[0][0] == board[1][0] == board[2][0] and board[0][0] != "-":
+        return board[0][0]
+    if board[0][1] == board[1][1] == board[2][1] and board[0][1] != "-":
+        return board[0][1]
+    if board[0][2] == board[1][2] == board[2][2] and board[0][2] != "-":
+        return board[0][2]
+    # Checking Diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != "-":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != "-":
+        return board[0][2]
     return False
 
 
 def check_filled(board):
-    return False
+    for row in board:
+        for elem in row:
+            if elem == "-":
+                # Board Not Empty
+                return False
+    return True
 
 
 if __name__ == "__main__":
     game_board = create_board()
     print_board(game_board)
-    x, y = get_input()
-    game_board[2][2] = "X"
-    print(check_valid(0, 0, game_board))
-    print(check_valid(1, 2, game_board))
-    print(check_valid(0, 4, game_board))
-    print(check_valid(5, 1, game_board))
+    # x, y = get_input()
+    # print(check_valid(0, 0, game_board))
+    # print(check_valid(1, 2, game_board))
+    # print(check_valid(0, 4, game_board))
+    # print(check_valid(5, 1, game_board))
+    turn_count = 0
+    while not check_winner(game_board) and not check_filled(game_board):
+        if turn_count % 2 == 0:
+            print("X's turn")
+            x, y = get_input()
+            if check_valid(x, y, game_board):
+                game_board[x][y] = "X"
+                print_board(game_board)
+        else:
+            print("O's turn")
+            x, y = get_input()
+            if check_valid(x, y, game_board):
+                game_board[x][y] = "O"
+                print_board(game_board)
+        turn_count += 1
 
-    while not check_winner and not check_filled:
-        exit()
+    # Check Winner
+    if check_winner(game_board):
+        print(f"Game Winner: {check_winner(game_board)}")
+    elif check_filled(game_board):
+        print("Game Tied")
+    else:
+        "Something Happened"
+
+  #  exit()
+
+
+  # Go again if spot is already taken
+  # Input Validation for spot choice (only accepts Ints right now)
+  # Build AI
+  # Pulling Down Other Project via Git
